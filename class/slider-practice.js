799bfe3f885710e247386da1fcb602697013b1b9
chocho030 <chocho0003@naver.com>
2025-05-07 22:19:19 +0900

    slider 수업-실습

--- class/slider-practice.js
+++ class/slider-practice.js
@@ -24,4 +24,32 @@ document.addEventListener("mousemove", (e) => {
   x = Math.max(0, Math.min(x, react.width));
 
   dot.style.left = `${x}px`;
+
+  if (x < 50) {
+    r = 255;
+    g = 0 + x * 5.5;
+    b = 0;
+  } else if (x < 100) {
+    r = 255 - (x - 50) * 5.5;
+    g = 255;
+    b = 0;
+  } else if (x < 150) {
+    r = 0;
+    g = 255;
+    b = 0 + (x - 100) * 5.5;
+  } else if (x < 200) {
+    r = 0;
+    g = 255 - (x - 150) * 5.5;
+    b = 255;
+  } else if (x < 250) {
+    r = 0 + (x - 200) * 5.5;
+    g = 0;
+    b = 255;
+  } else if (x < 300) {
+    r = 255;
+    g = 0;
+    b = 255 - (x - 250) * 5.5;
+  }
+
+  dot.style.backgroundColor = `rgb(${r},${g},${b})`;
 });
